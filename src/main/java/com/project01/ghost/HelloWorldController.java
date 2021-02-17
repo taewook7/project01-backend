fe5ceeb7b795c70c7f9a52cf77ac063355221c7f
xodnr1531 <xodnr1531@gmail.com>
2021-02-17 22:59:20 +0900

    testproject

---
+++ src/main/java/com/project01/ghost/HelloWorldController.java
@@ -0,0 +1,10 @@
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("pls", "The End");
+        return "hello";
+    }
+
+}
